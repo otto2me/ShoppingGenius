9a9d3707215e287a896891a28119d308c710e521
Oliver Otto <oliver.otto@cariad.technology>
2026-08-28 10:54:25 +0200

    fix build warning issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rendox.shoppinggenius"
         minSdk = 21
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.1.7"
+        versionCode = 12
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "com.rendox.shoppinggenius.testing.ShoppingGeniusTestRunner"
         vectorDrawables {
